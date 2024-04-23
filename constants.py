520642e1bc6fbc03f112ff8e976070958969cdc1
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-04-23 18:26:33 +0400

    constants

---
+++ constants.py
@@ -0,0 +1,24 @@
+FPS = 90
+FONT_SIZE = 50
+GAME_NAME = 'M\'nster Runner'
+GAME_MESSAGE = 'Press SPACE to start the game'
+MONSTER_TIMER_DELAY = 750
+MONSTER_TYPES = ['bird', 'crab', 'crab', 'crab', 'bird', 'crab', 'demon']
+
+# coordinates
+BG_COLOR = (125, 129, 120)
+GROUND_POS = (0, 300)
+SCORE_TEXT_COLOR = (64, 64, 64)
+SCREEN_MSG_CENTER = (400, 300)
+SCREEN_SIZE = (800, 400)
+SKY_POS = (0, 0)
+START_IMAGE_CENTER = (400, 200)
+TEXT_COLOR = (111, 196, 169)
+TITLE_CENTER = (400, 50)
+
+# paths
+FONT_PATH = 'static/font/AncientModern.ttf'
+GROUND_IMAGE_PATH = 'static/graphics/background/ground.png'
+SKY_IMAGE_PATH = 'static/graphics/background/sky.png'
+START_IMAGE_PATH = 'static/graphics/player/start_image.png'
+
